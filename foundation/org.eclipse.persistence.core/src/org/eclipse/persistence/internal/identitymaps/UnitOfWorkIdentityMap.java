c6e3c0ea0d3f7f76465e0c99b02521170b31fc3e
Vikas Shivamurthy <vshivamurthy@rallydev.com>
2014-09-10 09:54:24 -0600

    UnitOfWOrkIdentityMap uses HashMap again Watch for deadlocks

--- foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/internal/identitymaps/UnitOfWorkIdentityMap.java
+++ foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/internal/identitymaps/UnitOfWorkIdentityMap.java
@@ -13,8 +13,6 @@
 package org.eclipse.persistence.internal.identitymaps;
 
 import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
-
 import org.eclipse.persistence.descriptors.ClassDescriptor;
 import org.eclipse.persistence.internal.sessions.AbstractSession;
 
@@ -33,7 +31,7 @@ public class UnitOfWorkIdentityMap extends FullIdentityMap {
         super();
         this.maxSize = size;
         // PERF: Use a HashMap as more efficient than a ConcurrentMap and single threaded.
-        this.cacheKeys = new ConcurrentHashMap(size);
+        this.cacheKeys = new HashMap(size);
         this.descriptor = descriptor;
         this.session = session;
         this.isIsolated = isolated;
